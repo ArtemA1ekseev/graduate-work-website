e2cb3d915c0444d6a1baac252bd0980a6e43e402
qwsq4 <qwsq1437@gmail.com>
2023-03-12 19:42:34 +0800

    Фикс удаления товаров

--- src/main/java/ru/skypro/homework/service/impl/AdsServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AdsServiceImpl.java
@@ -9,23 +9,31 @@ import ru.skypro.homework.entity.Ads;
 import ru.skypro.homework.exception.AdvertNotFoundException;
 import ru.skypro.homework.exception.UserNotFoundException;
 import ru.skypro.homework.mapper.AdsMapper;
+import ru.skypro.homework.repository.AdsCommentRepository;
+import ru.skypro.homework.repository.AdsImageRepository;
 import ru.skypro.homework.repository.AdsRepository;
 import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.service.AdsService;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class AdsServiceImpl implements AdsService {
 
     private final AdsRepository adsRepository;
     private final UserRepository userRepository;
+    private final AdsCommentRepository commentRepository;
+    private final AdsImageRepository imageRepository;
     private final AdsMapper adsMapper;
 
-    public AdsServiceImpl(AdsRepository adsRepository, UserRepository userRepository, AdsMapper adsMapper) {
+    public AdsServiceImpl(AdsRepository adsRepository, UserRepository userRepository, AdsCommentRepository commentRepository, AdsImageRepository imageRepository, AdsMapper adsMapper) {
         this.adsRepository = adsRepository;
         this.userRepository = userRepository;
+        this.commentRepository = commentRepository;
+        this.imageRepository = imageRepository;
         this.adsMapper = adsMapper;
     }
 
@@ -49,6 +57,8 @@ public class AdsServiceImpl implements AdsService {
     @Override
     public void removeAds(Integer id) {
         Ads ads = adsRepository.findById(id).orElseThrow(AdvertNotFoundException::new);
+        imageRepository.deleteAllByAdsId(ads.getId());
+        commentRepository.deleteAllByAdsId(ads.getId());
         adsRepository.delete(ads);
     }
 
