b0896af1ca653d7d91cd694c762942a85bf4fa37
Sajer24 <sajer.kanat1992@gmail.com>
2023-03-19 23:53:06 +0300

    Изменил AUTH_WHITELIST

--- src/main/java/ru/skypro/homework/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/WebSecurityConfig.java
@@ -21,8 +21,8 @@ public class WebSecurityConfig {
             "/webjars/**",
             "/login",
             "/register",
-            "GET/ads",
-            "GET/ads/images/**"
+            "GET /ads",
+            "GET /ads/images/**"
     };
 
     @Bean
