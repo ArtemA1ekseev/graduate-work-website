e7c24931ed043753b1991bb1ca1a4d5dcb2c95a5
qwsq4 <qwsq1437@gmail.com>
2023-03-21 12:33:11 +0800

    Фикс

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -21,8 +21,8 @@ public class WebSecurityConfig {
             "/webjars/**",
             "/login",
             "/register",
-            "GET /ads",
-            "GET /ads/images/**"
+            "/ads",
+            "/ads/images/**"
     };
 
     @Bean
